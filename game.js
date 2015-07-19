415f3d212e36d5526410ee33c538255d096418b4
Jeremy Fouriaux <jeremy.fouriaux@gmail.com>
2015-07-20 00:14:35 +0800

    finalize display Map, need now debug from Electron to watch what s happened

--- game.js
+++ game.js
@@ -16,9 +16,87 @@ var map_1=[
 "###############"
 ];
 
-
 var actorChars = {
   "@": Player
+};
+
+var scale = 64;
+
+/*==================
+  DOM Helpers
+====================*/
+
+function elem(name, className) {
+  var elem = document.createElement(name);
+  if (className) elem.className = className;
+  return elem;
+}
+
+function DOMDisplay(parent, level) {
+  this.wrap = parent.appendChild(elem("div","game"));
+  this.level = level;
+
+  this.wrap.appendChild(this.drawBackground());
+  this.actorLayer = null;
+  this.drawFrame();
+}
+
+DOMDisplay.prototype.drawBackground = function () {
+  var table = elem ("table", "background");
+  table.style.width = this.level.width*scale + "px";
+  this.level.grid.forEach(function(row){
+    var rowElem = table.appendChild(elem("tr"));
+    rowElem.style.height = scale + "px";
+    row.forEach(function(type) {
+      rowElem.appendChild(elem("td", type));
+    });
+  });
+  return table;
+};
+
+DOMDisplay.prototype.drawActors = function () {
+  var wrap = elem("div");
+  this.level.actors.forEach(function(actor){
+    var rect = wrap.appendChild(
+      elem("div", "actor " + actor.type));
+    rect.style.width  = actor.size.x * scale + "px";
+    rect.style.height = actor.size.y * scale + "px";
+    rect.style.left   = actor.pos.x * scale + "px";
+    rect.style.top    = actor.pos.y * scale + "px";
+  });
+  return wrap;
+};
+
+DOMDisplay.prototype.drawFrame = function () {
+  if (this.actorLayer)
+   this.wrap.removeChild(this.actorLayer);
+  this.actorLayer = this.wrap.appendChild(this.drawActors());
+  this.wrap.className = "game" + (this.level.status || "");
+  this.scrollPlayerIntoView();
+};
+
+
+DOMDisplay.prototype.scrollPlayerIntoView = function () {
+var width = this.wrap.clientWidth;
+var height = this.wrap.clientHeight;
+var margin = width / 3;
+// The viewport
+var left = this.wrap.scrollLeft, right = left + width;
+var top = this.wrap.scrollTop, bottom = top + height;
+var player = this.level.player;
+var center = player.pos.plus(player.size.times(0.5)).times(scale);
+if (center.x < left + margin)
+this.wrap.scrollLeft = center.x - margin;
+else if (center.x > right - margin)
+this.wrap.scrollLeft = center.x + margin - width;
+if (center.y < top + margin)
+this.wrap.scrollTop = center.y - margin;
+else if (center.y > bottom - margin)
+this.wrap.scrollTop = center.y + margin - height;
+};
+
+DOMDisplay.prototype.clear = function () {
+this.wrap.parentNode.removeChild (this.wrap );
 };
 
 /*==================
@@ -82,8 +160,3 @@ function Level (plan) {
 Level.prototype.isFinished = function() {
   return this.status != null && this.finishDelay < 0;
 };
-
-
-var simpleLevel = new Level(map_1);
-console.log(simpleLevel.width);
-console.log(simpleLevel.height);
