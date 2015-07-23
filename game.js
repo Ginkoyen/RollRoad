b784d3a59d4ccddbcfd50741e6726628fd1f542b
Jeremy Fouriaux <jeremy.fouriaux@gmail.com>
2015-07-24 00:01:46 +0800

    add game loop and event handlers

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+/*==================
+  Globals
+====================*/
+
 var map_1=[
 "###############",
 "#             #",
@@ -16,11 +20,83 @@ var map_1=[
 "###############"
 ];
 
+
 var actorChars = {
   "@": Player
 };
-
+var GAME_LEVELS = [map_1];
 var scale = 32;
+var maxStep = 0.05;
+var playerSpeed = 7;
+var arrowCodes = {37: "left", 38: "up", 39:"right", 40:"down"};
+
+/*==================
+  Animation Helper Function
+====================*/
+function runAnimation (frameFunc) {
+  var lastTime = null;
+  function frame(time) {
+    var stop = false;
+    if (lastTime != null) {
+      var timeStep = Math.min(time - lastTime, 100) /1000;
+      stop = frameFunc(timeStep) === false;
+    }
+    lastTime = time;
+    if (! stop)
+      requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+}
+
+var arrows = trackKeys(arrowCodes);
+
+function runLevel (level, Display, andThen) {
+  var display = new Display(document.body, level);
+  runAnimation(function(step) {
+    level.animate(step, arrows);
+    display.drawFrame(step);
+    if (level.isFinished()) {
+      display.clear();
+      if (andThen)
+        andThen(level.status);
+      return false;
+    }
+  });
+}
+
+function runGame (plans, Display) {
+  function startLevel(n) {
+    runLevel(new Level(plans[n]), Display, function(status){
+      if (status == "lost")
+       startLevel(n);
+      else if (n < plans.length -1)
+        startLevel(n+1);
+      else
+        console.log("You win !");
+    });
+  }
+  startLevel(0);
+};
+
+
+/*==================
+  Event Handler
+====================*/
+
+function trackKeys (codes) {
+  var pressed = Object.create(null);
+  function handler(event) {
+    if (codes.hasOwnProperty(event.keyCode)) {
+      var down = event.type == "keydown";
+      pressed[codes[event.keyCode]] = down;
+      event.preventDefault();
+    }
+  }
+  addEventListener ("keydown", handler);
+  addEventListener ("keyup", handler);
+  return pressed;
+}
+
 
 /*==================
   DOM Helpers
@@ -122,10 +198,52 @@ function Player (pos) {
   this.pos = pos.plus(new Vector(0,0));
   this.size = new Vector (1,1);
   this.speed = new Vector (0,0);
+  this.direction = new Vector (0,0);
 }
 
 Player.prototype.type = "player";
+Player.prototype.move = function (step, level, keys){
+if(keys.left)
+{
+  this.direction.x = -1;
+  this.direction.y = 0;
+}
+if (keys.right)
+{
+  this.direction.x = 1;
+  this.direction.y = 0;
+}
+if (keys.up)
+{
+  this.direction.x = 0;
+  this.direction.y = -1;
+}
+if (keys.down)
+{
+  this.direction.x = 0;
+  this.direction.y = 1;
+}
 
+var motion = new Vector (this.speed.x * this.direction.x * step,
+  this.speed.y * this.direction.y * step);
+var newPos = this.pos.plus(motion);
+var obstacle = level.obstacleAt(newPos, this.size);
+if (obstacle)
+  level.playerTouched(obstacle);
+else
+ this.pos = newPos;
+};
+
+Player.prototype.act = function (step, level, keys) {
+  this.move(step,level,keys);
+  var otherActor = level.actorAt (this);
+  if (otherActor)
+    level.playerTouched(otherActor.type, otherActor);
+  if (level.status == "lost") {
+    this.pos.y += step;
+    this.size.y -= step;
+  }
+};
 
 /*==================
   Level "class" a constructor + added prototypes
@@ -160,3 +278,53 @@ function Level (plan) {
 Level.prototype.isFinished = function() {
   return this.status != null && this.finishDelay < 0;
 };
+
+// Detect if Player have a colision with a wall
+Level.prototype.obstacleAt = function (pos,size) {
+  var xStart = Math.floor(pos.x);
+  var xEnd = Math.ceil(pos.x+ size.x);
+  var yStart = Math.floor(pos.y);
+  var yEnd = Math.ceil (pos.y+size.y);
+  if (xStart < 0 || xEnd > this.width || yStart < 0 || yEnd > this.height)
+    return "wall";
+
+  for (var y=yStart; y < yEnd; y++){
+    for (var x=xStart; x < xEnd; x++){
+      var fieldType = this.grid[y][x];
+      if (fieldType == "wall") return fieldType;
+    }
+  }
+}
+
+// Look if an Actor overlap with the one given in parameter
+Level.prototype.actorAt = function(actor) {
+  for (var i=0; i < this.actors.length; i++) {
+    var other = this.actors[i];
+    if (other != actor &&
+      actor.pos.x + actor.size.x > other.pos.x &&
+      actor.pos.x < other.pos.x + other.size.x &&
+      actor.pos.y + actor.size.y > other.pos.y &&
+      actor.pos.y < other.pos.y + other.size.y)
+      return other;
+  }
+};
+
+Level.prototype.animate = function(step, keys) {
+  if (this.status != null)
+    this.finishDelay -= step;
+  while (step > 0) {
+    var thisStep = Math.min(step, maxStep);
+    this.actors.forEach(function(actor){
+      actor.act(thisStep, this, keys);
+    }, this);
+    step -= thisStep;
+  }
+};
+
+Level.prototype.playerTouched = function(type, actor) {
+  if (type == "wall" && this.status == null)
+  {
+    this.status = "lost";
+    this.finishDelay = 1;
+  }
+}
